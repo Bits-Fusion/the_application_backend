a70f0394f83a9c802b759211607a981d7ba753d5
unknown <workumyfather@gmail.com>
2025-07-10 21:59:37 +0300

    config: database config

---
+++ config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+	"log"
+	"strings"
+	"sync"
+)
+
+type Config struct {
+	Server *Server
+	Db     *Db
+}
+
+type Server struct {
+	Port int
+}
+
+type Db struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+	TimeZone string
+}
+
+var (
+	once           sync.Once
+	configInstance *Config
+)
+
+func GetConfig() *Config {
+	once.Do(func() {
+		// Load .env file into environment variables
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Warning: .env file not found, relying on environment variables only")
+		}
+
+		viper.AutomaticEnv()
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+		configInstance = &Config{
+			Server: &Server{
+				Port: viper.GetInt("SERVER_PORT"),
+			},
+			Db: &Db{
+				Host:     viper.GetString("DB_HOST"),
+				Port:     viper.GetInt("DB_PORT"),
+				User:     viper.GetString("DB_USER"),
+				Password: viper.GetString("DB_PASSWORD"),
+				DBName:   viper.GetString("DB_DBNAME"),
+				SSLMode:  viper.GetString("DB_SSLMODE"),
+				TimeZone: viper.GetString("DB_TIMEZONE"),
+			},
+		}
+	})
+
+	return configInstance
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/Bits-Fusion/the_application_backend/config"
+	"github.com/Bits-Fusion/the_application_backend/database"
+	"github.com/Bits-Fusion/the_application_backend/server"
+)
+
+func main() {
+	conf := config.GetConfig()
+
+	db := database.NewPostgresDatabase(conf)
+	server.NewEchoServer(conf, db).Start()
+}
